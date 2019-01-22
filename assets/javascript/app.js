618cd8c926ab8aef22bf9fb85b645d6b5cf1a6b9
Santino <ha.agharbi@gmail.com>
2019-01-21 18:11:52 -0800

    fixed moment format and optimized code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,10 +12,10 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-    // Create event handler for 'Submit' click
+    // Event handler to add train schedule
     $("#add-train-button").on("click", function (event) {
         event.preventDefault();
-
+        
         //Create object with properties
         //set to input field values
         var train = {
@@ -32,23 +32,15 @@ $(document).ready(function () {
     });
 
     function currentTime() {
-        var current = moment().format('LT');
+        var current = moment().format('HH:mm');
         $("#currentTime").html(current);
         setTimeout(currentTime, 1000);
-
-        console.log(current);
     };
 
+    // Grabs new added train schedules
     database.ref().on(
         "child_added",
         function(snapshot) {
-
-            var startTimeConverted = moment(snapshot.val().time, "hh:mm").subtract(1, "years");
-            var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
-            var timeRemain = timeDiff % snapshot.val().frequency;
-            var minToArrival = snapshot.val().frequency - timeRemain;
-            var nextTrain = moment().add(minToArrival, "minutes");
-            var key = snapshot.key;
 
             var train = snapshot.val();
             console.log("child:", train);
@@ -64,31 +56,51 @@ $(document).ready(function () {
             console.log(trainTime);
             console.log(trainFreq);
 
-            // var departTime = moment.unix(trainTime).format("HH:MM");
+            // Convert start time 
+            var startTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
             
-            // var minutesAway = moment().diff(moment(trainTime, "X"), "HH:MM");
+            // Calculate difference between times in minutes
+            var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+            
+            // Calculate the remainder in frequency
+            var timeRemain = timeDiff % trainFreq;
 
-            // var minutesAway = trainTime - currentTime;
-            // console.log(minutesAway);
+            // Calculate Minutes to next train
+            var minToArrival = trainFreq - timeRemain;
+
+            // Next Train time
+            var nextTrain = moment().add(minToArrival, "minutes");
 
 
-            //Append to the table row to display employee data
-
-
+            // Append to the table row to display employee data
             var addTrainRow = $("#add-train-row");
             var newTrain = $("<tr>").append(
                 $("<td>").text(trainName),
                 $("<td>").text(trainDest),
                 $("<td>").text(trainFreq),
-                $("<td>").text(moment(nextTrain).format("LT")),
+                $("<td>").text(moment(nextTrain).format("HH:mm")),
                 $("<td>").text(minToArrival),
+                $("<td>").html("<i id='update' class='fa fa-edit' style='color:blue' aria-hidden='true'></i>"),
+                $("<td>").html("<i id='close' class='fa fa-times' style='color:red' aria-hidden='true'></i>")
             );
                 $(addTrainRow).append(newTrain);
-            
+
+                // Removes train schedule entry 
+                $('table').on('click', '#close', function(e){
+                $(this).closest('tr').remove()
+                })
+
+                // Delete from Firebase(pending)
+
+                // Edit train schdule entry (pending)
         }
+        
     )
     
     currentTime();
-
     
+    // Update train schedule once every minute
+    setInterval(function () {
+        window.location.reload();
+    }, 60000);
 });
